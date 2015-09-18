c76a227afcdcd439fd4c06db989425207c497453
Ewen <hwl@beecloud.cn>
2015-09-18 18:37:29 +0800

    update

--- BCPaySDK/BeeCloud/Internal/BCPayUtil.m
+++ BCPaySDK/BeeCloud/Internal/BCPayUtil.m
@@ -71,7 +71,6 @@
 
 + (NSString *)getBestHostWithFormat:(NSString *)format {
     NSString *verHost = [NSString stringWithFormat:@"%@%@",kBCHosts[arc4random()%kBCHostCount],reqApiVersion]; //2015.07.28
-    verHost = @"http://58.211.191.122:8080/1";
     return [NSString stringWithFormat:format, verHost];
 }
 
